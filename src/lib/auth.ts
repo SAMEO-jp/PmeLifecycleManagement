61ff06df1d1f3ef854805be2dd56e5bfc9dac8b5
SAMEO-jp <sameo-jp@users.noreply.github.com>
2025-10-29 15:33:36 +0900

    Reorganize auth structure and add API routes

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,18 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db"; // your drizzle instance
+import { nanoid } from "nanoid";
+
 
 export const auth = betterAuth({
+    baseUrl: "http://localhost:3000",
     database: drizzleAdapter(db, {
         provider: "pg", // or "mysql", "sqlite"
-        usePlural: true,
+        usePlural: true,// テーブルを複数形
     }),
+    adovanced: {
+        database: {
+            generateId: () => nanoid(10),// 10文字のランダムなIDを生成
+        },
+    },
 });
